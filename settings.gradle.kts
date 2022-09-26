f371c6700e5a0621bb9b3c5f6817541c422324f2
Enda Phramoedya <phramoedyaenda@gmail.com>
2022-09-26 09:41:58 +0700

    first commit

---
+++ settings.gradle.kts
@@ -0,0 +1,11 @@
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Kotlin_Test_App"
+include(":androidApp")
+include(":shared")
---
+++ shared/src/commonMain/kotlin/com/example/kotlintestapp/Platform.kt
@@ -0,0 +1,5 @@
+package com.example.kotlintestapp
+
+expect class Platform() {
+    val platform: String
+}
